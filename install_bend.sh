18e02aad03ffe933d82eba4aca073dd746d9dc3a
Lurpigi <luigiestebant@gmail.com>
2025-03-19 13:30:48 +0100

    added nvidia runtime

--- install_bend.sh
+++ install_bend.sh
@@ -13,4 +13,8 @@ echo "Installing hvm and bend-lang..."
 cargo install hvm
 cargo install bend-lang
 
+echo "Installing NVIDIA runtime..."
+wget https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda_12.8.1_570.124.06_linux.run
+sudo sh cuda_12.8.1_570.124.06_linux.run
+
 echo "Installation complete!"
